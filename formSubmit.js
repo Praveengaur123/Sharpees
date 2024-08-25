5ed597f438131308c21ae63b77bafbe0742df775
Praveen Gaur <140590555+Praveengaur123@users.noreply.github.com>
2024-08-25 19:01:13 +0530

    pushed code for all api request crud

--- formSubmit.js
+++ formSubmit.js
@@ -6,46 +6,78 @@ function handleFormSubmit(event){
        email:event.target.email.value,
        phone:event.target.phone.value
     }
+    //post requset
     axios
-    .post("https://crudcrud.com/api/0637a81530f743679663fe723e0d8f1d/todos",
+    .post("https://crudcrud.com/api/80d7aad262484abc9407bb43604a38ec/todo",
         userDetails)
         .then((response)=> displayUserOnScreen(response.data))
-        .catch((err)=>console.log(err));
+        .catch((err)=>{
+            document.body.innerHTML=document.body.innerHTML+"<h4>Something Mystery</h4>"
+            console.log(err)
+        });
+    
     //clearing the output
     document.getElementById("username").value="";
     document.getElementById("phone").value="";
     document.getElementById("email").value="";
-    
-}
+    //document.body.innerHTML
+    }
+//get requset
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get("https://crudcrud.com/api/80d7aad262484abc9407bb43604a38ec/todo")
+    .then((response)=>{
+        console.log(response)
+        for(var i=0;i<response.data.length;i++)
+        {
+            displayUserOnScreen(response.data[i])
+        }
+    })
+})
 
     // display user on screen
     function displayUserOnScreen(userDetails){
         const userItem=document.createElement("li")
+        // assign the iD
+        userItem.id=userDetails._id
         userItem.appendChild(
             document.createTextNode(
                 `${userDetails.username}-${userDetails.email}-${userDetails.phone}`
             )
         )
+        //creating delete button
         const deleteBtn=document.createElement("button")
         deleteBtn.appendChild(document.createTextNode("Delete"))
         userItem.appendChild(deleteBtn)
-
+        // creating edit button
         const editBtn=document.createElement("button")
         editBtn.appendChild(document.createTextNode("Edit"))
         userItem.appendChild(editBtn)
-
+        // delete button functinality
         deleteBtn.addEventListener("click" ,function(event){
-            userList.removeChild(event.target.parentElement)
-            localStorage.removeItem(userDetails.email)
+            axios.delete(`https://crudcrud.com/api/80d7aad262484abc9407bb43604a38ec/todo/${userDetails._id}`)
+            .then((res)=>{
+                displayUserOnScreen(res.data)
+                    userList.removeChild(event.target.parentElement)
         })
+                .catch((err)=>console.log(err));
+            })
+        
+        // edit button functinality
         editBtn.addEventListener("click",function(event){
-            userList.removeChild(event.target.parentElement)
-            localStorage.removeItem(userDetails.email)
+            localStorage.removeItem(userDetails)
             document.getElementById("username").value=userDetails.username;
             document.getElementById("phone").value=userDetails.phone;
             document.getElementById("email").value=userDetails.email;
+            axios.delete(`https://crudcrud.com/api/80d7aad262484abc9407bb43604a38ec/todo/${userDetails._id}`)
+            .then((res)=>{
+                displayUserOnScreen(res.data)
+                    userList.removeChild(event.target.parentElement)
         })
-
+                .catch((err)=>console.log(err));
+            
+        })
         const userList=document.querySelector("ul")
         userList.appendChild(userItem)
     }
+
+    
